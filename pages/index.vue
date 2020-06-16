4865c6cb10f8637926a7affc044c90bd73afebb2
ColinTravis <colint@cybertravis.us>
2020-06-15 19:15:35 -0500

    Fixed spacing

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="bg-black h-screen font-stolzl">
     <div class="container mx-auto h-full flex justify-center items-center">
-      <div class="w-auto sm:w-auto md:w-2/3 px-4">
+      <div class="w-auto md:w-2/3 px-4">
         <div class="border-white border-2 p-4 mb-6 rounded-lg shadow-xl">
           <h1 class="text-center mb-8 text-white font-jura font-thin tracking-widest">Jeralise Tylke</h1>
           <logo class="mx-auto block w-1/3 text-white" />
           <div class="my-8">
             <h2 class="text-white font-semibold font-sans text-center">{{emailAddress}}</h2>
           </div>
-          <div class="flex flex-row items-center justify-center space-x-20 my-4">
+          <div class="flex flex-row items-center justify-center space-x-6 md:space-x-16 my-4">
             <a href="https://www.instagram.com/jera_tylkz/" target="_blank" aria-label="Instagram Link" title="Instagram" rel="noopener">
               <Icon-Instagram class="transition-colors duration-300 ease-in-out text-center w-8 text-white hover:text-purple-500" />
             </a>
