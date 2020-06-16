bc3e6d95c97ba46af6022c2bbb108e4ae561593b
ColinTravis <colint@cybertravis.us>
2020-06-15 19:11:15 -0500

    Added instagram icon

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,9 +1,11 @@
 import IconArtStation from './IconArtStation.vue';
 import IconVimeo from './IconVimeo.vue';
+import IconInstagram from './IconInstagram.vue';
 import IconWix from './IconWix.vue';
 
 export {
   IconArtStation,
   IconVimeo,
-  IconWix
+  IconWix,
+  IconInstagram
 };
